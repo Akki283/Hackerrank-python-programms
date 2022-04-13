d78fd42e0ed7271fddff7533caeb6601418de8de
Akki283 <89306780+Akki283@users.noreply.github.com>
2022-04-13 20:59:38 +0530

    Update string_wraping.py

--- string_wraping.py
+++ string_wraping.py
@@ -10,3 +10,14 @@ if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
     print(result)
+############## alternate logic ######################
+a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+b = 4
+print(a[0],end='')
+for i in range(1,len(a)):
+
+    print(a[i],end='')
+    if i%b == 0:
+        if i == 0:
+            continue
+        print()
